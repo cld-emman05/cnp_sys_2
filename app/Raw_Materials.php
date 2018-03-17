d29aa12d53471d9f7f9d8a7ed62a90b360a1c91a
clikus <clifford_martinez@dlsu.edu.ph>
2018-03-17 13:36:44 +0800

    Version 7.3.1
    Trail update on Raw Material Model

--- app/Raw_Materials.php
+++ app/Raw_Materials.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Raw_Materials extends Model
 {
-    //
+    //specs
+    public function specs(){
+      return $this->belongsTo('App\Specs');
+    }
+
+    //Suppliers
+    public function supplier(){
+      return $this->belongsTo('App\Supplier');
+    }
 }
