9ca65f76e95f62029f75bd7ca12c28f55f080f0f
bastag <30362063+bastag@users.noreply.github.com>
2018-04-09 19:07:35 +0800

    latest admin dashboard
    updated admin dashboard

--- resources/views/dashboard/administrator.blade.php
+++ resources/views/dashboard/administrator.blade.php
@@ -9,43 +9,13 @@
 	<div class="row">
 		<div class="col-md-6">
 				<div class="card card-chart">
-						<div class="card-header">
-							<i class="card-category">Greetings, {{ auth::user()->first_name }} {{ auth::user()->last_name }} !</i>
-								<h4 class="card-title">Top agents</h4>
-							</div>
-							<div class="card card-chart">
-								<table class="table table-hover">
-									<thead>
-										<tr>
-											<th>Sales Agent</th>
-											<th>Orders made</th>
-											<th>Total sales (PhP)</th>
-										</tr>
-									</thead>
 
-									<tbody>
-										<tr>
-											<td>De La Salle University</td>
-											<td><a href = 'order/view'>1</a></td>
-											<td>PhP 30</td>
-										</tr>
-									</tbody>
-								</table>
+
 						</div>
 				</div>
 			</div>
 
-			<div class="col-md-6">
-					<div class="card card-chart">
-							<div class="card-header">
-									<h4 class="card-title">Time check</h4>
-									<h5 class="card-category"> <i class = "date"> </i></h5>
-								</div>
-								<div class="card-body">
-									<h1><b class="clock"></b></h1>
-								</div>
-					</div>
-				</div>
+
 			</div>
 
 			<div class="row">
@@ -79,29 +49,7 @@
 
 					<div class="col-lg-6">
 						<div class="card card-chart">
-								<div class="card-header">
-									<i class="card-category">Greetings, {{ auth::user()->first_name }} {{ auth::user()->last_name }} !</i>
-										<h4 class="card-title">Popular Job Orders</h4>
-									</div>
-									<div class="card card-chart">
-										<table class="table table-hover">
-											<thead>
-												<tr>
-													<th>Job Type</th>
-													<th>Orders made</th>
-													<th>Total sales</th>
-												</tr>
-											</thead>
 
-											<tbody>
-												<tr>
-													<td>Yearbook</td>
-													<td><a href = 'order/view'>1</a></td>
-													<td>PhP 30</td>
-												</tr>
-											</tbody>
-										</table>
-								</div>
 						</div>
 					</div>
 			</div>
@@ -119,6 +67,7 @@
 												<tr>
 													<th>Reasons</th>
 													<th>Customers</th>
+													<th>Date</th>
 												</tr>
 											</thead>
 
@@ -126,6 +75,7 @@
 												<tr>
 													<td>Job price is expensive</td>
 													<td>1</a></td>
+													<td>April 5, 2019</a></td>
 												</tr>
 											</tbody>
 										</table>
@@ -133,4 +83,116 @@
 						</div>
 					</div>
 	</div>
+
+
+
+	<div class="row">
+			<div class="col-lg-6">
+				<div class="card card-chart">
+						<div class="card-header">
+							<i class="card-category">Greetings, {{ auth::user()->first_name }} {{ auth::user()->last_name }} !</i>
+								<h4 class="card-title">Debt Tracker</h4>
+							</div>
+							<div class="card card-chart">
+								<table class="table table-hover">
+									<thead>
+										<tr>
+											<th>Supplier</th>
+											<th>Total Balance</th>
+											<th>Term Due</th>
+										</tr>
+									</thead>
+
+									<tbody>
+										<tr>
+											<td><a href= '#'>Dhunwell</a></td>
+											<td><a href= '#'>Php 500</a></td>
+											<td><a href= '#'>{{Carbon\Carbon::now()->format('M d, Y')	}}</a></td>
+										</tr>
+									</tbody>
+								</table>
+						</div>
+				</div>
+			</div>
+</div>
+
+<div class="row">
+		<div class="col-lg-6">
+			<div class="card card-chart">
+					<div class="card-header">
+						<i class="card-category">Greetings, {{ auth::user()->first_name }} {{ auth::user()->last_name }} !</i>
+							<h4 class="card-title">Cashflow</h4>
+						</div>
+						<div class="card card-chart">
+							<table class="table table-hover">
+								<thead>
+									<tr>
+										<th>Date</th>
+																<th>Source</th>
+																<th>Amount</th>
+																<th>Method</th>
+
+									</tr>
+								</thead>
+
+								<tbody>
+									<tr>
+																<td><a href= '#'>{{Carbon\Carbon::now()->format('M d, Y')	}}</a></td>
+																<td><a href= '#'>Crisostomo Ibarra</a></td>
+																<td><a href= '#'>Php 500</a></td>
+																<td><a href= '#'>Inflow</a></td>
+															</tr>
+
+															<tr>
+																<td><a href= '#'>{{Carbon\Carbon::now()->format('M d, Y')	}}</a></td>
+																<td><a href= '#'>Dhunwell</a></td>
+																<td><a href= '#'>Php 250</a></td>
+																<td><a href= '#'>Outflow</a></td>
+															</tr>
+
+								</tbody>
+							</table>
+					</div>
+			</div>
+		</div>
+</div>
+
+<div class="row">
+		<div class="col-lg-6">
+			<div class="card card-chart">
+					<div class="card-header">
+						<i class="card-category">Greetings, {{ auth::user()->first_name }} {{ auth::user()->last_name }} !</i>
+							<h4 class="card-title">Profit</h4>
+						</div>
+						<div class="card card-chart">
+							<table class="table table-hover">
+								<thead>
+									<tr>
+										<th>Date</th>
+																<th>Amount</th>
+																<th>Customer</th>
+
+									</tr>
+								</thead>
+
+								<tbody>
+									<tr>
+																<td><a href= '#'>{{Carbon\Carbon::now()->format('M d, Y')	}}</a></td>
+																<td><a href= '#'>De La Salle Green and White</a></td>
+																<td><a href= '#'>Php 500000</a></td>
+															</tr>
+
+															<tr>
+																<td><a href= '#'>{{Carbon\Carbon::now()->format('M d, Y')	}}</a></td>
+																<td><a href= '#'>SPRAWL MMA</a></td>
+																<td><a href= '#'>Php 250000</a></td>
+															</tr>
+
+								</tbody>
+							</table>
+					</div>
+			</div>
+		</div>
+</div>
+
 @endsection
